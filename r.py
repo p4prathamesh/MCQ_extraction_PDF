27c0dd55fc360eef6aa74adf791f7cdd73f29e5c
Prathamesh Wagh <pratham.wagh1998@gmail.com>
2020-07-03 19:27:29 +0530

    Add files via upload

---
+++ r.py
@@ -0,0 +1,16 @@
+import pdfplumber
+import re
+pdf = pdfplumber.open("The_Living_World.pdf")
+l=len(pdf.pages)
+
+for i in range(0,l):
+	page = pdf.pages[i]
+	text = page.extract_text()
+
+	questions = re.compile(r'^[0-9]. *')
+	mcq = re.compile(r'^[(][0-9][)] *')
+	sol = re.compile(r'^[S][o][l][.] *')
+	for line in text.split('\n'):
+		if questions.match(line) or mcq.match(line) or sol.match(line):
+			print(line)
+	
